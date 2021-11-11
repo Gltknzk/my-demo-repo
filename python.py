6bd2d33bf4cc5092d02d45f4e5b33dbfd81e18f6
zeki <gltknzk@gmail.com>
2021-11-11 19:50:00 +0300

    python.py oluşturuldu

---
+++ python.py
@@ -0,0 +1,6 @@
+name = input("please enter your first name=")
+my_name = "zack"
+if name == my_name:
+      print("Hello, Zack!The password is 123456")
+  else:
+        print("Hello {}! See you later".format(name))
